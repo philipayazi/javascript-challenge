122375628fdb26255e2637b839e666eae1b5e24c
Philip Ayazi <philipayazi@gmail.com>
2020-01-12 15:59:34 -0600

    update

--- ET-level-1/static/js/app.js
+++ ET-level-1/static/js/app.js
@@ -10,11 +10,23 @@ var button = d3.select('#filter-btn')
 button.on('click', function(){
     // access the text box in the html
     var textBox = d3.select('#datetime');
+    var textCity = d3.select('#city');
+    var textState = d3.select('#state');
+    var textCountry = d3.select('#country');
+    var textShape = d3.select('#shape');
     // extract value from text box
     var dataValue = textBox.property('value');
+    var dataCity = textCity.property('value');
+    var dataState = textState.property('value');
+    var dataCountry = textCountry.property('value');
+    var dataShape = textShape.property('value');
 
-    // check to see if date in text box matches date in data table
+    // check to see if value in text box matches value in data table
     var matchedDate = tableData.filter(date => (date.datetime === dataValue));
+    var matchedCity = tableData.filter(city => (city.city === dataCity));
+    var matchedState = tableData.filter(state => (state.state === dataState));
+    var matchedCountry = tableData.filter(country => (country.country === dataCountry));
+    var matchedShape = tableData.filter(shape => (shape.shape === dataShape));
 
     // check to see if I can access the data in data.js
     console.log(tableData);
@@ -22,6 +34,10 @@ button.on('click', function(){
     console.log(dataValue);
     // check to see if data is filtered
     console.log(matchedDate);
+    console.log(matchedCity);
+    console.log(matchedState);
+    console.log(matchedCountry);
+    console.log(matchedShape);
 
     // locate table html tag
     var tbody = d3.select('tbody')
@@ -29,10 +45,30 @@ button.on('click', function(){
     // clear all data from table before inserting new data
     tbody.html("");
 
-    // place each value on an object into the table
+    // place each value of an object in the array into the table
     matchedDate.forEach(date => {
         var row = tbody.append('tr')
         Object.values(date).forEach( value => row.append('td').text(value));
+    });
+
+    matchedCity.forEach(city => {
+        var row = tbody.append('tr')
+        Object.values(city).forEach( value => row.append('td').text(value));
+    });
+
+    matchedState.forEach(state => {
+        var row = tbody.append('tr')
+        Object.values(state).forEach( value => row.append('td').text(value));
+    });
+
+    matchedCountry.forEach(country => {
+        var row = tbody.append('tr')
+        Object.values(country).forEach( value => row.append('td').text(value));
+    });
+
+    matchedShape.forEach(shape => {
+        var row = tbody.append('tr')
+        Object.values(shape).forEach( value => row.append('td').text(value));
     });
 
 });
